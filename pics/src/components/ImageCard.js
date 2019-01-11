a8df0356b9bc532b5be08925e838d8f93655949f
Richard <richard@tlgd.co.uk>
2019-01-11 17:49:42 +0000

    componentDidMount console log will return 0 for height at this point because the actual image hanst been downloaded yet

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render(){
         const { urls, description} = this.props.image;
         return (
             <img 
+                ref={this.imageRef}
                 src={urls.regular}
                 alt={description}
             />
