237e6954190430ca890ae6f3e5dac11e5d275ba6
Richard <richard@tlgd.co.uk>
2019-01-11 18:20:18 +0000

    Finshed image app, clever use of grid-auto-rows to give up finer grained control over the veritcal spacing

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,21 +4,33 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = { spans: 0 };
+
         this.imageRef = React.createRef();
     }
 
     componentDidMount() {
-        console.log(this.imageRef.current.clientHeight);
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans });
     }
 
     render(){
         const { urls, description} = this.props.image;
         return (
-            <img 
-                ref={this.imageRef}
-                src={urls.regular}
-                alt={description}
-            />
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img 
+                    ref={this.imageRef}
+                    src={urls.regular}
+                    alt={description}
+                />
+            </div>
         );
     }
 }
