372e49b679fc36ee1d35a1df594903d975e3be61
Richard <richard@tlgd.co.uk>
2019-01-16 17:59:32 +0000

    Init the google client library

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component{
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '428413434019-i3frf70euv7loop502taandola3jma2a.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+
     render() {
         return <div>Google Auth</div>
     };
