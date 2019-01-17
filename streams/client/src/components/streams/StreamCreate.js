d77140d8d95fdc7ba32605fa46b1f54e62f6e8f6
Richard <richard@tlgd.co.uk>
2019-01-17 18:55:16 +0000

    Create a new stream via a rest request in the createStream action creator after submitting the form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -26,9 +26,9 @@ class StreamCreate extends React.Component {
         );
     };
 
-    onSubmit(formValues) {
-        console.log(formValues);
-    }
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return(
@@ -55,7 +55,9 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
