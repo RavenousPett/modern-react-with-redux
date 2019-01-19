87de62cef3095fbb3dbe08ee8d383433fc1c1da6
Richard <richard@tlgd.co.uk>
2019-01-19 13:21:06 +0000

    Installed lodash, finished stream reducer

--- streams/client/src/reducers/steamReducer.js
+++ streams/client/src/reducers/steamReducer.js
@@ -1,4 +1,4 @@
-import _ from 'loadash';
+import _ from 'lodash';
 import {
     CREATE_STREAM,
     FETCH_STEAMS,
@@ -10,6 +10,8 @@ import streams from '../apis/streams';
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STEAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STEAM:
         case CREATE_STREAM:
         case EDIT_STREAM:
