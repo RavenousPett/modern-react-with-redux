5b87656423097c5e91385ad5898f6f42e0ead47d
Richard <richard@tlgd.co.uk>
2019-01-21 17:23:36 +0000

    Create rtmpserver directory and installed node-media-server in it. Copied config from github into index.js and added a start command to package.json

--- streams/client/src/reducers/steamReducer.js
+++ streams/client/src/reducers/steamReducer.js
@@ -6,7 +6,6 @@ import {
     DELETE_STREAM,
     EDIT_STREAM
 } from '../actions/types';
-import streams from '../apis/streams';
 
 export default (state = {}, action) => {
     switch (action.type) {
@@ -15,7 +14,7 @@ export default (state = {}, action) => {
         case FETCH_STEAM:
         case CREATE_STREAM:
         case EDIT_STREAM:
-            return { ...streams, [action.payload.id]: action.payload };
+            return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
             return _.omit(state, action.payload); 
         default:
