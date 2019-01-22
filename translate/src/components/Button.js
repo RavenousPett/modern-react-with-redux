96eb50fe27c58762b2a7c84b16f0456a5f89dad9
Richard <richard@tlgd.co.uk>
2019-01-22 13:12:21 +0000

    Example pulling from multiple context components. Have to use consumers for this

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColourContext from '../contexts/ColourContext';
 
 class Button extends React.Component {
     renderSubmit(value) {
         return value === 'english' ? 'Submit' : 'Voorleggen';
     }
 
-    render() {
+    renderButton(colour) {
         return(
-            <button className="ui button primary">
+            <button className={`ui button ${colour}`}>
                 <LanguageContext.Consumer>
                     {(value) => this.renderSubmit(value)}
                 </LanguageContext.Consumer>
-            </button>
+            </button>   
+        );
+    }
+
+
+    render() {
+        return(
+            <ColourContext.Consumer>
+                {(colour) => this.renderButton(colour)}
+            </ColourContext.Consumer>
         );
     }
 }
