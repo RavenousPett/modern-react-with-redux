0c64e311172d12cb6133d6a581a35dbff5e1ad73
Richard <richard@tlgd.co.uk>
2022-04-21 15:05:51 +0100

    list building

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -26,6 +26,19 @@ const Search = () => {
     // (async () => { await axios.get('dfsfsf')})();
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -36,6 +49,7 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+        <div className="ui celled list">{renderedResults}</div>
       </div>
     </div>
   );
